9ab67c21ba5923afe4ed0feb1049af44ff120c13
Ye Myat Thu <thu.yemyat@gmail.com>
2014-10-15 22:37:28 +0630

    make circle imageview and other changes

--- app/src/main/java/com/yemyatthu/ourdays/activity/MyConfigActivity.java
+++ app/src/main/java/com/yemyatthu/ourdays/activity/MyConfigActivity.java
@@ -1,22 +1,22 @@
 package com.yemyatthu.ourdays.activity;
 
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.yemyatthu.ourdays.R;
 import com.yemyatthu.ourdays.fragment.MyConfigFragment;
 
-public class MyConfigActivity extends Activity {
+public class MyConfigActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_config);
-        FragmentManager fm = getFragmentManager();
+        FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
         if(fragment == null){
             fm.beginTransaction().add(R.id.fragment_container,new MyConfigFragment()).commit();}
--- app/src/main/java/com/yemyatthu/ourdays/fragment/MyConfigFragment.java
+++ app/src/main/java/com/yemyatthu/ourdays/fragment/MyConfigFragment.java
@@ -1,59 +1,90 @@
 package com.yemyatthu.ourdays.fragment;
 
 
-import android.app.Fragment;
+
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RemoteViews;
 
 
-import com.pkmmte.view.CircularImageView;
+import com.cengalabs.flatui.FlatUI;
+import com.squareup.picasso.Picasso;
 import com.yemyatthu.ourdays.R;
+import com.yemyatthu.ourdays.util.CircleTransform;
 import com.yemyatthu.ourdays.widget.LoveWidgetProvider;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * A simple {@link Fragment} subclass.
  *
  */
 public class MyConfigFragment extends Fragment {
-    private EditText loveText;
-    private Button loveButton;
-    private CircularImageView testImage;
+    @InjectView(R.id.set_love_text) EditText loveText;
+    @InjectView(R.id.update_text_button) Button loveButton;
+    @InjectView(R.id.main_male)ImageView mainMale;
+    @InjectView(R.id.main_female) ImageView mainFemale;
 
 
     public MyConfigFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FlatUI.initDefaultValues(getActivity());
+        FlatUI.setDefaultTheme(R.array.candy);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_my_config,container,false);
-        loveText = (EditText) v.findViewById(R.id.set_love_text);
-        loveButton = (Button) v.findViewById(R.id.update_text_button);
-
+        ButterKnife.inject(this,v);
+        Picasso.with(getActivity())
+                .load(R.drawable.doge)
+                .transform(new CircleTransform())
+                .into(mainMale);
+        Picasso.with(getActivity())
+                .load(R.drawable.doge)
+                .transform(new CircleTransform())
+                .into(mainFemale);
         loveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Context context = getActivity();
                 AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
                 RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-                ComponentName thisWidget = new ComponentName(context,LoveWidgetProvider.class);
+                ComponentName thisWidget = new ComponentName(context, LoveWidgetProvider.class);
                 remoteViews.setTextViewText(R.id.together_text, loveText.getText().toString());
+                remoteViews.setImageViewBitmap(R.id.male_picture,circleBitmap(R.drawable.doge));
+                remoteViews.setImageViewBitmap(R.id.female_picture,circleBitmap(R.drawable.doge));
                 appWidgetManager.updateAppWidget(thisWidget, remoteViews);
             }
         });
         return v;
     }
+
+    private Bitmap circleBitmap(int resource){
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),resource);
+        Bitmap newBitmap = new CircleTransform().transform(bitmap);
+        return newBitmap;
+    }
+
 
 
 }
